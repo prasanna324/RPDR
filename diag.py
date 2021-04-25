3dddbd88333157c141be7efe667b76a61106041a
mssher07 <marc.s.sherman@gmail.com>
2021-04-25 19:31:22 -0400

    small update, meds, diag, removed meds from lftlib

--- diag.py
+++ diag.py
@@ -1,5 +1,39 @@
 
-def load_diag(path, delimiter='|', datetime_col='Date', prune=True):
+def load_RPDR_diag_multiple(dir_data, filename_diag, delimiter='|'):
+    ''' load_RPDR_diag_multiple(dir_data, filename_diag, delimiter='|', make_lower=True):
+        Sequentially loads all files from RPDR data dump when multiple files have the same name. 
+        
+        1. Starts in dir_data (should have trailing slash), grabs all sub-folders' names automatically, then sequentially loads: dir_data/[sub-folders]/path_labs (where path_labs is the name of the file)
+        * Note for whatever reason, on a multiple-split file dump from RPDR the labs, demographics, etc files are all named the exact same, just in different zips
+        2. Calls the traditional load meds function on each file
+        3. Concatenates all results and returns 1 DF
+        4. If make_lower==True, converts all medication names to lower case
+        
+        Warnings:
+        1. Do not have any other subfolders besides the ones containing data in dir_data
+        
+        '''
+    import os
+    import pandas as pd
+    
+    # get list of subdirectories
+    subdirectories = [x[0] for x in os.walk(dir_data)][1:]
+    
+    first=True
+    # for each subdir, use the traditional load function to load data and concat
+    for subdir in subdirectories:
+        path_to_diag_full=subdir+'/'+filename_diag
+        diag = load_RPDR_diag(path=path_to_diag_full, delimiter=delimiter)
+        
+        if first==True:
+            concat_pd = diag
+            first=False
+        else:
+            concat_pd=pd.concat([concat_pd, diag],ignore_index=True)
+        
+    return concat_pd
+
+def load_RPDR_diag(path, delimiter='|', datetime_col='Date', prune=True):
     import pandas as pd
     
     diag = pd.read_csv(path, delimiter=delimiter, dtype=str)
